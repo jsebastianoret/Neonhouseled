ed3d4dad138e86ffd31d17ea2f7de64070d13d5c
Frank Puertas <frankgzmc@gmail.com>
2024-08-10 09:19:58 -0500

    bd

--- admin2/config/db.php
+++ admin2/config/db.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST', 'localhost');
-define('NAME', 'root');
-define('PASS', '');
-define('DB', 'ghxumdmy_digimediamkt');
+define('NAME', 'ghxumdmy_NHLUser');
+define('PASS', 'AQR-EvieoU)3');
+define('DB', 'ghxumdmy_NeonHouseLed');
 //Datos importantes
 
 
