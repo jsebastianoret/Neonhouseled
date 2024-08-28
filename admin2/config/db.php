081c7cc2b01261969d5c35a7d2fa38b77b81519c
Frank Puertas <frankgzmc@gmail.com>
2024-08-28 11:34:11 -0500

    Merge

--- admin2/config/db.php
+++ admin2/config/db.php
@@ -5,10 +5,3 @@ define('PASS', 'AQR-EvieoU)3');
 define('DB', 'ghxumdmy_NeonHouseLed');
 //Datos importantes
 
-try {
-    $conn = new PDO("mysql:host=" . HOST . ";dbname=" . DB, NAME, PASS);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Conexión fallida: " . $e->getMessage());
-}
-?>
