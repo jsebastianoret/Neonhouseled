3c9f3bd3b50cab8050c5f24bd68a013045d68362
Frank Puertas <frankgzmc@gmail.com>
2024-08-28 12:03:43 -0500

    Ruta alterna IMG

--- admin2/public/view/utils/sidebar.php
+++ admin2/public/view/utils/sidebar.php
@@ -138,6 +138,7 @@
         src="./../../img/user-admin.png"
         alt=""
         height="50px"
+        onError="this.onerror=null; this.src='./admin2/public/img/user-admin.png';"
       />
       <div class="info-usuario">
         <div class="nombre-email">
