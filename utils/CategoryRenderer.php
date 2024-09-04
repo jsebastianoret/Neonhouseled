ec7e3666d579d3e3d74fa2ac4c6e207d23c7e3b4
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-09-04 10:59:25 -0500

    feat: Css crítico, optimización de recursos y código

--- utils/CategoryRenderer.php
+++ utils/CategoryRenderer.php
@@ -8,7 +8,7 @@ class CategoryRenderer {
         return "<div class=\"col\">
             <a href=\"vistas/categorias/{$category['title']}.php\" class=\"card-link\">
                 <div class=\"image-container\">
-                    <img class=\"lazyLoad\" id=\"imagen_{$category['id']}\" src=\"public/imagenes/categoria/BACKUP/{$category['image']}\" alt=\"{$category['name']}\">
+                    <img loading=\"lazy\" class=\"lazyLoad\" id=\"imagen_{$category['id']}\" src=\"public/imagenes/categoria/BACKUP/{$category['image']}\" alt=\"{$category['name']}\">
                      <div class=\"overlay__card\">
                             <h2 class=\"card-title\">{$category['name']}</h2>
                         </div>
