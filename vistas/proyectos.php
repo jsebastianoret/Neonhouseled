059815736541ba1c3cb70e7b280d4f144bbb8c67
kimberly31-HC <kimberlyhuamancris31@gmail.com>
2024-09-05 10:56:53 -0500

    MODIFICACIONES

--- vistas/proyectos.php
+++ vistas/proyectos.php
@@ -46,29 +46,30 @@ session_start();
             <p>"Tendencia, vanguardia y novedades"</p>
         </section>
        
-<section class="pry-nuevos-container">
-            <h2>Proyectos nuevos</h2>
-            <div class="pry-nuevos-desc">
-                <div class="pry">
-                    <figure class="img-container">
-                        <a href="https://youtu.be/qU08CNcnQhA" target="_blank">
-                            <img src="../public/imagenes/proyectos/1.jpeg" alt="Imagen del selvicio">
-                        </a>
-                    </figure>
-                    <p>Decoración e iluminación <br>
-                    de interiores y exteriores</p>
-                </div>
-                <div class="pry">
-                    <figure class="img-container">
-                        <a href="https://youtu.be/3QMkxPf7H3U" target="_blank">
-                            <img src="../public/imagenes/proyectos/2.jpeg" alt="Imagen del selvicio">
-                        </a>
-                    </figure>
-                    <p>Decoración con pantallas <br>
-                    LED</p>
-                </div>
-            </div>
-        </section>
+        <section class="pry-nuevos-container">
+    <h2>Proyectos nuevos</h2>
+    <div class="pry-nuevos-desc">
+        <div class="pry">
+            <figure class="img-container">
+                <a href="https://youtu.be/qU08CNcnQhA" target="_blank">
+                    <img src="../public/imagenes/proyectos/1.WEBP" alt="Imagen del servicio">
+                </a>
+            </figure>
+            <p>Decoración e iluminación <br>
+            de interiores y exteriores</p>
+        </div>
+        <div class="pry">
+            <figure class="img-container">
+                <a href="https://youtu.be/3QMkxPf7H3U" target="_blank">
+                    <img src="../public/imagenes/proyectos/2.WEBP" alt="Imagen del servicio">
+                </a>
+            </figure>
+            <p>Decoración con pantallas <br>
+            LED</p>
+        </div>
+    </div>
+</section>
+
 
     </main>
     <br>
