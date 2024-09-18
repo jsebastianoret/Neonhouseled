07e7a09d2e034951d1750e7b853e37b7d5712cfa
J Sebastian Ore <118024431+jsebastianoret@users.noreply.github.com>
2024-09-18 12:27:09 -0500

    Update login.php

--- admin/view/login.php
+++ admin/view/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
   <head><meta charset="euc-jp">
@@ -7,12 +6,14 @@
     <title>Inicio de sesion</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <link rel="stylesheet" href="public/css/login.css">
+    <link rel="stylesheet" href="/Neonhouseled/public/bootstrap/bootstrap.min.css">
     <link
       href="https://unpkg.com/boxicons@2.0.5/css/boxicons.min.css"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="/Neonhouseled/public/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/login.css">
+
+    
   </head>
   <body>
   <div class="login-container">
@@ -28,7 +29,7 @@
                 <div class="linkt-div">
                     <h2>Bienvenido</h2>
                     <a href="../index.php" class="link-index">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-house-fill" viewBox="0 0 16 16">
                             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
                             <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
                         </svg>
@@ -58,5 +59,3 @@
   <script src="js/login.js"></script>
   </body>
 </html>
-
-
